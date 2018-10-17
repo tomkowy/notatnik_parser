54314724a9ee054bc4b60007c27f6cc3da815916
TomekM <o2.tomek@tlen.pl>
2018-10-17 17:50:22 +0200

    Szkielet aplikacji #4 - project init, main menu init info

---
+++ Notatnik_parser/Notatnik_parser/MainMenu/MainMenu.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Notatnik_parser.MainMenu
+{
+    public class MainMenu
+    {
+        public void Run()
+        {
+            ShowInitInfo();
+
+            Console.ReadKey();
+        }
+
+        public void ShowInitInfo()
+        {
+            Console.WriteLine("Witaj w aplikacji Notatnik/Parser");
+            Console.WriteLine("Wybierz co chcesz robić:");
+            Console.WriteLine("1 - notatnik");
+            Console.WriteLine("2 - parser XML");
+            Console.WriteLine("3 - kreator XML");
+        }
+    }
+}
---
+++ Notatnik_parser/Notatnik_parser/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Notatnik_parser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var mainMenu = new Notatnik_parser.MainMenu.MainMenu();
+            mainMenu.Run();
+        }
+    }
+}
