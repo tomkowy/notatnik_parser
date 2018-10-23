34be18782538ad4217d97a7f5563ad09f6cb089a
GrzegorzDryja <GrzegorzDryja.via@gmail.com>
2018-10-23 23:18:01 +0200

    issue#5
    SaveFile function to create empty LastNote.txt file in \...\bin\Debug cataloge.

--- Notatnik_parser/Notatnik_parser/Notepad/NotepadMenu.cs
+++ Notatnik_parser/Notatnik_parser/Notepad/NotepadMenu.cs
@@ -1,12 +1,21 @@
 ﻿using System;
+using System.IO;
 
 namespace Notatnik_parser.Notepad
 {
     public class NotepadMenu
     {
+        public void SaveFile()
+        {            
+            string fileName = "LastNote.txt";            
+            using (System.IO.FileStream fs = System.IO.File.Create(fileName))
+            {
+
+            }
+        }
         public void Run()
         {
-            Console.WriteLine("Witamy w notatniku");
-        }
+            Console.WriteLine("Witamy w notatniku");        
+        }      
     }
 }
