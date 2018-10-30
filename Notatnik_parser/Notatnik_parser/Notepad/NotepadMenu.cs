bd1ecc30f1161f7ba7359968ea77708f1103ca12
GrzegorzDryja <GrzegorzDryja.via@gmail.com>
2018-10-30 22:32:44 +0100

    Issue#5 & #19
    Coding, coding....

--- Notatnik_parser/Notatnik_parser/Notepad/NotepadMenu.cs
+++ Notatnik_parser/Notatnik_parser/Notepad/NotepadMenu.cs
@@ -1,21 +1,33 @@
 ﻿using System;
 using System.IO;
+using System.Text;
 
 namespace Notatnik_parser.Notepad
 {
     public class NotepadMenu
-    {
+    {   
+        string note;
+
         public void SaveFile()
         {            
-            string fileName = "LastNote.txt";            
+            string fileName = "LastNote.txt"; 
             using (System.IO.FileStream fs = System.IO.File.Create(fileName))
             {
-
+                Byte[] toSave = new UTF8Encoding(true).GetBytes(note);
+                fs.Write(toSave, 0, toSave.Length);
             }
         }
+
         public void Run()
         {
-            Console.WriteLine("Witamy w notatniku");        
+            Console.WriteLine("Witamy w notatniku. Wprowadź notatkę (wciśnij <enter> aby zapisać):");
+            note = Console.ReadLine();
+
+            ConsoleKeyInfo saveButton = Console.ReadKey();
+            if (saveButton.Key == ConsoleKey.Enter)
+            {
+                SaveFile();
+            }
         }      
     }
 }
