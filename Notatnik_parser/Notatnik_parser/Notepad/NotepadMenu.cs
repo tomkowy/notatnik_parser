0cb4bf7c3f6363df7ef8cd3bd0ead15e04715de3
GrzegorzDryja <GrzegorzDryja.via@gmail.com>
2018-11-15 23:16:33 +0100

    issue#22 first atempt
    SaveFile function is asking user to input full path with file name.
    I've made try catch exception for UnauthorizedAccessException but it doesn't return any statement to user if note is save were he wish or by default. Will work  on it next time.

--- Notatnik_parser/Notatnik_parser/Notepad/NotepadMenu.cs
+++ Notatnik_parser/Notatnik_parser/Notepad/NotepadMenu.cs
@@ -6,15 +6,37 @@ namespace Notatnik_parser.Notepad
 {
     public class NotepadMenu
     {           
-        private void SaveFile(string textNoteParameter)
+        private bool SaveFile(string textNoteParameter)
         {            
-            string fileName = "LastNote.txt";
+            string pathAndFileName;
+            
+            Console.WriteLine("Podaj pełną ścieżkę z nazwą pliku:");
+            pathAndFileName = Console.ReadLine();
 
-            using (FileStream fs = File.Create(fileName))
+            try
+            {
+                using (FileStream fs = File.Create(pathAndFileName))
                 {
                     Byte[] textNote = new UTF8Encoding(true).GetBytes(textNoteParameter);
                     fs.Write(textNote, 0, textNote.Length);
-                }            
+                }
+            }
+            catch (UnauthorizedAccessException)
+            {
+                FileAttributes attr = (new FileInfo(pathAndFileName)).Attributes;
+                Console.Write("Błędna ścieżka.");
+            }
+            finally
+            {
+                string defaultFileName = "Domyślna_notatka.txt";
+                using (FileStream fs = File.Create(defaultFileName))
+                {
+                    Byte[] textNote = new UTF8Encoding(true).GetBytes(textNoteParameter);
+                    fs.Write(textNote, 0, textNote.Length);
+                }
+            }
+
+            return true;
         }
 
         public void Run()
@@ -26,11 +48,10 @@ namespace Notatnik_parser.Notepad
             note = Console.ReadLine();
 
             ConsoleKeyInfo typedKey = Console.ReadKey();
-            if (typedKey.Key == ConsoleKey.Enter)
-            {
-                Console.WriteLine("To jest twoja notatka, która została zapisana do pliku txt: " + note);
-                SaveFile(note);
-                
+            if (typedKey.Key == ConsoleKey.Enter && SaveFile(note))
+            {   
+                Console.WriteLine("Zapisano.");
+                                
                 var mainMenu = new Notatnik_parser.MainMenu.MainMenu();
                 mainMenu.Run();
             }            
